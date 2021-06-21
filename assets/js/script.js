78147709dcdef1a1dd681ab484ce3481e00245da
Paola <paola62010@gmail.com>
2021-06-21 17:53:30 +0100

    Add function to flip cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,28 @@
-function flipCard {
+let cards = document.getElementsByClassName("memory-card");
+let unflippedCard = true;
+let lock = false;
+let cardOne;
+let cardTwo;
+
+for (let card of cards) {
+    card.addEventListener("click", flipCard);
+    
+}
+
+function flipCard() {
+    if (lock) return;
+    if (this === cardOne) return;
+
+    this.classList.toggle("flip");
+
+    if (unflippedCard) {
+        unflippedCard = false;
+        cardOne = this;
+    }
+    else {
+        cardTwo = this;
+        checkMatches();
+    }
 
 }
 
@@ -15,5 +39,5 @@ function unflipCard {
 }
 
 function resetBoard {
-    
+
 }
